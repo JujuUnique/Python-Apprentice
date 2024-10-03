5d901cd981a5ef189de88ac64aa014dbc3ad5304
JujuUnique <163606771+JujuUnique@users.noreply.github.com>
2024-10-03 01:24:27 +0000

    wheeeeeeeeeeeeeeeee

--- lessons/00_Turtles/09_LeagueBot.py
+++ lessons/00_Turtles/09_LeagueBot.py
@@ -5,16 +5,33 @@ Write your own turtle program! Here is what your program should do
 1) Change the turtle image to 'leaguebot_bolt.gif'
 2) Change the turtle size to 10x10
 3) Change the turtle line color to 'blue'
-4) Draw a hexagon using a loop and variables. 
-
+4) Draw a hexagon using a loop and variables.
 """
 
-import turtle as turtle
-
+import turtle
+def set_turtle_image(turtle, image_name):
+    from pathlib import Path
+    image_dir = Path(__file__).parent / "images"
+    image_path = str(image_dir / image_name)
+    screen = turtle.getscreen()
+    screen.addshape(image_path)
+    turtle.shape(image_path)
 screen = turtle.Screen()
 screen.setup(width=600, height=600)
+t = turtle.Turtle()
+set_turtle_image(t, 'leaguebot_bolt.gif')
+
 screen.bgcolor('white')
 
-t = turtle.Turtle()
+t.turtlesize(stretch_wid=10, stretch_len=10, outline=4)
+
+t.pencolor('blue')
+def draw_polygon(sides):
+    angle = 360/sides
+    for i in range(sides):
+        t.forward(50)
+        t.left(angle)
+draw_polygon(6)
+turtle.exitonclick()
 
 ... # Your Code Here
--- lessons/00_Turtles/10a_Tash_Me.py
+++ lessons/00_Turtles/10a_Tash_Me.py
@@ -3,11 +3,22 @@
 Write a program that:
 1) Loads an emoji image as the background
 2) Make the turtle shape a moustach
-3) Move the moustache to the right spont on the emoji
+3) Move the moustache to the right spot on the emoji
 
 Hint: See 08a_More Turtle Programs, section 'Change the Background Image' and
 'Change the Turtle Shape'
 """
 
 ... # Your code here
+import turtle
+def set_turtle_image(turtle,image_name):
+    from pathlib import Path
+    image_dir = Path(__file__).parent / "images"
+    image_path = dir(image_dir / image_name)  
+    screen = turtle.getscreen()
+    screen.addshape(image_path)
+screen=turtle.screen()
+screen.setup(width=600, hight=600)
+t=turtle.Turtle()
 
+screen.bgcolor('white')
