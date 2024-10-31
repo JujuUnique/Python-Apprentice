e4b6e32c6929942bd8758ffec35cd11551acc7f9
JujuUnique <163606771+JujuUnique@users.noreply.github.com>
2024-10-31 01:13:51 +0000

    asdfghjk

--- lessons/00_Turtles/10b_Tash_Me_Click.py
+++ lessons/00_Turtles/10b_Tash_Me_Click.py
@@ -6,7 +6,55 @@
 # Hint: See 08a_More Turtle Programs, section 'Click on the Screen'
  
 ... # Your code here
+
 import turtle
 def set_turtle_image(turtle,image_name):
-    from pathlib import path
-    image_dir = path(__file__).parent / "images"
+    from pathlib import Path
+    image_dir = Path(__file__).parent / "images"
+    image_path = str(image_dir / image_name)
+    screen=turtle.getscreen()
+    screen.addshape(image_path)
+    turtle.shape(image_path)
+
+screen=turtle.Screen()
+screen.setup(width=600, height=600)
+t=turtle.Turtle()
+set_turtle_image(t, 'moustache1.gif')
+screen.bgcolor('white')
+t.turtlesize(stretch_wid=3, stretch_len=3, outline=4)
+
+def set_backround_image(window, image_name):
+    from pathlib import Path
+    from PIL import Image
+    image_dir=Path(__file__).parent/"images"
+    image_path=str(image_dir/image_name)
+    image=Image.open(image_path)
+    window.setup(image.width, image.height, startx=0, starty=0)
+    window.bgpic(image_path)
+
+set_backround_image(screen, "emoji.png")
+t.penup()
+def screen_clicked(x, y):
+    t.goto(x, y)
+
+screen.onclick(screen_clicked)
+
+turtle.done()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
