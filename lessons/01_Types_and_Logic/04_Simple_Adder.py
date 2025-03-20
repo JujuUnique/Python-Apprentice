c9908ba10183aabad00fd385754b87d4fbdf8a77
JujuUnique <163606771+JujuUnique@users.noreply.github.com>
2025-03-20 01:28:38 +0000

    meep

--- lessons/01_Types_and_Logic/04_Simple_Adder.py
+++ lessons/01_Types_and_Logic/04_Simple_Adder.py
@@ -10,16 +10,20 @@ at the comments and the code snippets in the previous lessons, like
 """
 
 # Import the required modules
-
+from tkinter import messagebox, simpledialog, Tk
 # Create a window object
+window=Tk()
+window.withdraw()
 
 # Hide the window, hint: use the withdraw method
 
 # Ask the user for the first number   
-
+number=simpledialog.askinteger("", "give a number")
 # Ask the user for the second number
-
+number2=simpledialog.askinteger("","another number")
 # Display the sum of the two numbers 
+messagebox.showinfo('',"your numbers equal " +str(number+number2))
+# Keep the window open oh yeah also click trash otherwise it wont work lol
+#window.mainloop()
 
-# Keep the window open
 
