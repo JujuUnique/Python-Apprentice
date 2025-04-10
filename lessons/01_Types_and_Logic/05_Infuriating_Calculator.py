b23739690dc97771e95d526de925c15ceafd18e2
JujuUnique <163606771+JujuUnique@users.noreply.github.com>
2025-04-10 01:25:27 +0000

    fhxhgffc

--- lessons/01_Types_and_Logic/05_Infuriating_Calculator.py
+++ lessons/01_Types_and_Logic/05_Infuriating_Calculator.py
@@ -22,17 +22,19 @@ you can ask for a string with simpledialog.askstring().
 """
 
 # Import the required modules
-
+from tkinter import messagebox, simpledialog, Tk
 # Create a window object
-
 # Hide the window, hint: use the withdraw method
+window=Tk 
+window.withdraw()
+
 
 # Ask the user for the first number   
-
+number=simpledialog.askinteger("","give a number")
 # Ask the user for the second number
-
+number2=simpledialog.askinteger("","again")
 # Ask the user for the math operation
-
+operation=("","operation")
 # Use if-elif-else statements to provide the desired math operation on the numbers and display the result.
 
 # If the user enters an unknown operation, display an error message. ( use messagebox.showerror()
